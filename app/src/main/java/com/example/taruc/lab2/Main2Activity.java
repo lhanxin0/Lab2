30d32ec35c23bf5c799f678569ac3d6ab9251734
lhanxin0 <leehx-pa16@student.tarc.edu.my>
2018-10-23 09:30:44 +0800

    Lab2 Excercise

---
+++ app/src/main/java/com/example/taruc/lab2/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.taruc.lab2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+}
